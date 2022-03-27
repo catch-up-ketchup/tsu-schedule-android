828116240276474b4cf4ff91f9ab90826ac3b892
Nikita Romanov <nikita.romanov@devexpress.com>
2022-03-27 21:45:55 +0300

    Add core files

--- app/src/main/java/com/mkamanin/tsu_schedule/TSUWebViewClient.kt
+++ app/src/main/java/com/mkamanin/tsu_schedule/TSUWebViewClient.kt
@@ -4,6 +4,7 @@ import android.view.View
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.Toast
 
 class TSUWebViewClient: WebViewClient() {
     // Redirect around workspace
@@ -18,5 +19,13 @@ class TSUWebViewClient: WebViewClient() {
     override fun onPageFinished(view: WebView?, url: String?) {
         super.onPageFinished(view, url)
         view?.visibility = View.VISIBLE
+    }
+
+    override fun onReceivedError(view: WebView?, errorCode: Int, description: String?, failingUrl: String?) {
+        view?.loadUrl("blank.htm")
+        view?.alpha = 0f
+        Toast.makeText(view?.context, "No internet connection :(", Toast.LENGTH_LONG).show()
+
+        //super.onReceivedError(view, errorCode, description, failingUrl)
     }
 }
